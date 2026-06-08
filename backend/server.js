453956344a52fad41bad78c2b94aa41d85fea8e6
arunpravin125 <162274540+arunpravin125@users.noreply.github.com>
2026-06-09 02:23:59 +0530

    deployment setup added in server.js

--- backend/server.js
+++ backend/server.js
@@ -8,12 +8,15 @@ import { cartRoutes } from "./routes/cartRoutes.js";
 import { couponsRoutes } from "./routes/couponsRoutes.js";
 import { paymentsRoutes } from "./routes/paymentsRoutes.js";
 import { analyticsRoutes } from "./routes/analyticsRoutes.js";
+import path from "path";
 
 const app = express();
 
 dotenv.config();
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
+
+const __dirname = path.resolve();
 
 const port = process.env.PORT || 5001;
 app.use("/api/auth", authRoutes);
@@ -22,6 +25,15 @@ app.use("/api/cart", cartRoutes);
 app.use("/api/coupons", couponsRoutes);
 app.use("/api/payments", paymentsRoutes);
 app.use("/api/analytics", analyticsRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
+
 app.listen(port, () => {
   ConnectMongooseDB();
   console.log(`Server is Running on ${port}`);
